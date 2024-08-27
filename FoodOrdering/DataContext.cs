e08f7a51c7b0b85651bec2a1a82e23bdb0c780b6
Jordan Woodruff <boogermanus@gmail.com>
2024-08-27 12:56:51 -0500

    data context and seeding worker

---
+++ FoodOrdering/DataContext.cs
@@ -0,0 +1,11 @@
+using FoodOrdering.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodOrdering.Contexts;
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+    public DbSet<FoodItem> FoodItems { get; set; }
+    public DbSet<Order> Orders { get; set; }
+}
