491a2be38380b20be8977af18668b6370dc8bc75
Jordan Woodruff <boogermanus@gmail.com>
2024-08-28 16:57:42 -0500

    customers component

--- FoodOrderingClient/src/app/app.routes.ts
+++ FoodOrderingClient/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CustomersComponent } from './pages/customers/customers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '/customers', component: CustomersComponent}
+];
---
+++ FoodOrderingClient/src/app/pages/customers/customers.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { FoodItem } from '../../interfaces/food-item';
+import { Order } from '../../interfaces/order';
+import { firstValueFrom, Subscription } from 'rxjs';
+import { RealtimeClientService } from '../../services/realtime-client.service';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-customers',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './customers.component.html',
+  styleUrl: './customers.component.css'
+})
+export class CustomersComponent implements OnInit {
+  public availableFood = signal<Array<FoodItem>>([]);
+  public activeOrders = signal<Array<Order>>([]);
+  public activeOrdersSubscription?: Subscription;
+  public tableNumber?: number;
+  public showActiveOrders: boolean = false;
+
+
+  constructor(
+    private readonly realtime: RealtimeClientService,
+    private readonly httpClient: HttpClient) {
+
+  }
+
+  async ngOnInit() {
+    let food =
+      await firstValueFrom<Array<FoodItem>>(
+        this.httpClient.get<Array<FoodItem>>('http://localhost:5215/api/FoodItems/GetFoodItems')
+      );
+
+    this.availableFood.set([...food]);
+
+    let orders =
+      await firstValueFrom<Array<Order>>(
+        this.httpClient.get<Array<Order>>('http://localhost:5215/api/Kitchen/GetExistingOrders')
+      );
+
+    this.activeOrders.set([...orders]);
+  }
+
+  async sendOrder(foodId: number, tableNumber: number) {
+    await this.realtime.orderFoodItem(foodId, tableNumber);
+  }
+
+  showActiveOrdersToggle() {
+    this.showActiveOrders = !this.showActiveOrders;
+  }
+}
